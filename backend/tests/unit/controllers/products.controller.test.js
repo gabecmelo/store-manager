d9c454606add85040029f1a089d87e8ad7a544a4
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-11-21 18:45:35 -0300

    adiciona testes para a rota /products

---
+++ backend/tests/unit/controllers/products.controller.test.js
@@ -0,0 +1,36 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const { productsService } = require('../../../src/services');
+const { productsController } = require('../../../src/controllers');
+
+const {
+  productsFromModel,
+  productsRecoveredFromService,
+} = require('../mocks/products.mock');
+
+const { expect } = chai;
+chai.use(require('sinon-chai'));
+
+describe('Realizando testes - PRODUCT CONTROLLER:', function () {
+  it('Recuperando todos os produtos com sucesso - status 200', async function () {
+    sinon
+      .stub(productsService, 'getProducts')
+      .resolves(productsRecoveredFromService);
+
+    const req = {
+      body: 'nothing here',
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.getAllProducts(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(productsFromModel);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
---
+++ backend/tests/unit/mocks/products.mock.js
@@ -0,0 +1,14 @@
+const productsFromModel = [
+  { id: 1, name: 'name' },
+  { id: 2, name: 'name' },
+];
+
+const productsRecoveredFromService = {
+  status: 'SUCCESSFULL',
+  data: productsFromModel,
+};
+
+module.exports = {
+  productsFromModel,
+  productsRecoveredFromService,
+};
