830ff98dbaefb64814090e6de3e75bff56913053
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-11-22 12:12:32 -0300

    corrige ordenacao

--- backend/src/models/products.model.js
+++ backend/src/models/products.model.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
 
 const findAll = async () => {
-  const [products] = await connection.execute('SELECT * FROM products');
+  const [products] = await connection.execute('SELECT * FROM products ORDER BY id');
   return products;
 };
 
 const findById = async (id) => {
   const [[product]] = await connection.execute(
-    `SELECT * FROM products WHERE id = ?
-    ORDER BY id`,
+    `SELECT * FROM products 
+    WHERE id = ?`,
     [id],
   );
 
