dbfff127d5c997898743fde5e91ee708f8804df8
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-11-24 12:50:09 -0300

    correcao chaves de testes

--- backend/tests/unit/mocks/sales.mock.js
+++ backend/tests/unit/mocks/sales.mock.js
@@ -1,21 +1,21 @@
 const salesFromModel = [
-  { saleId: 1, date: '2023-11-23 14:00:14', saleId: '1', quantity: 5 },
-  { saleId: 2, date: '2023-06-22 11:30:34', saleId: '2', quantity: 15 },
+  { saleId: 1, date: '2023-11-23 14:00:14', productId: '1', quantity: 5 },
+  { saleId: 2, date: '2023-06-22 11:30:34', productId: '2', quantity: 15 },
 ];
 
 const salesFromDB = [
-  { saleId: 1, date: '2023-11-23 14:00:14', saleId: '1', quantity: 5 },
-  { saleId: 2, date: '2023-06-22 11:30:34', saleId: '2', quantity: 15 },
+  { saleId: 1, date: '2023-11-23 14:00:14', productId: '1', quantity: 5 },
+  { saleId: 2, date: '2023-06-22 11:30:34', productId: '2', quantity: 15 },
 ];
 
 const saleFromModel = [
-  { date: '2023-11-23 18:18:18', saleId: '1', quantity: 7 },
-  { date: '2023-11-23 18:18:18', saleId: '2', quantity: 3 },
+  { date: '2023-11-23 18:18:18', productId: '1', quantity: 7 },
+  { date: '2023-11-23 18:18:18', productId: '2', quantity: 3 },
 ];
 
 const saleFromDB = [
-  { date: '2023-11-23 18:18:18', saleId: '1', quantity: 7 },
-  { date: '2023-11-23 18:18:18', saleId: '2', quantity: 3 },
+  { date: '2023-11-23 18:18:18', productId: '1', quantity: 7 },
+  { date: '2023-11-23 18:18:18', productId: '2', quantity: 3 },
 ];
 
 const salesRecoveredFromService = {
